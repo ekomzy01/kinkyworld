611c9b2e39e55b7cb49e36e3133062d26428e5f8
ekomzy <ekomzy@gmail.com>
2023-06-04 02:05:21 +0100

    postgres

--- bdsm/settings.py
+++ bdsm/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'bdsm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgresql_database',
+        'USER': 'postgres',
+        'PASSWORD': '001ekomzy',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
