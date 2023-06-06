144cd13c22a26f2ab88869ad25a2ac47f1a52fef
ekomzy <ekomzy@gmail.com>
2023-06-06 01:04:58 +0100

    update

--- products/urls.py
+++ products/urls.py
@@ -5,8 +5,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('cart/', views.cart, name='cart'),
     path('checkout/', views.checkout, name='checkout'),
+    path('payment/', views.payment, name="payment"),
 
     path('update_item/', views.updateItem, name="update_item"),
-	path('process_order/', views.processOrder, name="process_order"),
+    path('process_order/', views.processOrder, name="process_order"),
     path('details/<int:id>', views.details, name='details'),
 ]
